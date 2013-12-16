cda10357b5c5b3962097b66c5dc40d28d2b1057f
fpottier <fpottier@0f8b5475-4b4e-0410-85a8-ee3154a6bfe7>
2013-12-16 13:41:51 +0000

    Updated calc and its lexer for better style.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/menhir@349 0f8b5475-4b4e-0410-85a8-ee3154a6bfe7

--- demos/calc/calc.ml
+++ demos/calc/calc.ml
@@ -1,14 +1,28 @@
+let process (line : string) =
+  let linebuf = Lexing.from_string line in
+  try
+    (* Run the parser on this line of input. *)
+    Printf.printf "%d\n%!" (Parser.main Lexer.token linebuf)
+  with
+  | Lexer.Error msg ->
+      Printf.fprintf stderr "%s%!" msg
+  | Parser.Error ->
+      Printf.fprintf stderr "At offset %d: syntax error.\n%!" (Lexing.lexeme_start linebuf)
+
+let process (optional_line : string option) =
+  match optional_line with
+  | None ->
+      ()
+  | Some line ->
+      process line
+
+let rec repeat channel =
+  (* Attempt to read one line. *)
+  let optional_line, continue = Lexer.line channel in
+  process optional_line;
+  if continue then
+    repeat channel
+  
 let () =
-  let stdinbuf = Lexing.from_channel stdin in
-  while true do
-    (* Read line by line. *)
-    let linebuf = Lexing.from_string (Lexer.line stdinbuf) in
-    try
-      (* Run the parser on a single line of input. *)
-      Printf.printf "%d\n%!" (Parser.main Lexer.token linebuf)
-    with
-    | Lexer.Error msg ->
-	Printf.fprintf stderr "%s%!" msg
-    | Parser.Error ->
-	Printf.fprintf stderr "At offset %d: syntax error.\n%!" (Lexing.lexeme_start linebuf)
-  done
+  repeat (Lexing.from_channel stdin)
+
