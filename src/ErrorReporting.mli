385ae35fa67ae0dd6aa34df72e8245d861d0fa73
François Pottier <francois.pottier@inria.fr>
2015-01-22 14:16:10 +0100

    Updated ErrorReporting to extract positions out of the stack. Untested.

--- src/ErrorReporting.mli
+++ src/ErrorReporting.mli
@@ -17,17 +17,35 @@ module Make
   open I
 
   (* An explanation is a description of what the parser has recognized in the
-     recent past and what it expects next. For now, an explanation is just an
-     item. *)
+     recent past and what it expects next. *)
 
-  type explanation =
-      item
+  type 'symbol explanation = {
+
+    (* An explanation is based on an item. *)
+    item: item;
+
+    (* A past. This is a non-empty sequence of (terminal and non-terminal)
+       symbols, each of which corresponds to a range of the input file. These
+       symbols correspond to the first half (up to the bullet) of the item's
+       right-hand side. In short, they represent what we have recognized in
+       the recent past. *)
+    past: ('symbol * Lexing.position * Lexing.position) list;
+
+    (* A future. This is a non-empty sequence of (terminal and non-terminal)
+       symbols These symbols correspond to the second half (after the bullet)
+       of the item's right-hand side. In short, they represent what we expect
+       to recognize in the future, if this item is a good prediction. *)
+    future: 'symbol list;
+
+    (* A goal. This is a non-terminal symbol. It corresponds to the item's
+       left-hand side. In short, it represents the reduction that we will
+       be able to perform if we successfully recognize this future. *)
+    goal: 'symbol
+
+  }
 
   (* We build lists of explanations. These explanations may originate in
      distinct LR(1) states. *)
-
-  type explanations =
-      explanation list
 
   (* TEMPORARY *)
 
@@ -36,7 +54,7 @@ module Make
 
   (* TEMPORARY *)
 
-  exception Error of explanations
+  exception Error of xsymbol explanation list
 
   val entry: 'a I.result -> (Lexing.lexbuf -> token) -> Lexing.lexbuf -> 'a
 
