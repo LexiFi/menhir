0a077a78b1c7c34245df2bcb1c32e954788c0a27
François Pottier <francois.pottier@inria.fr>
2015-07-02 13:10:42 +0200

    Added the computation of the minimal length of each nonterminal. This information is displayed at log level -lg 2. Changed the display to not include the artificial start symbols.

---
+++ src/CompletedNat.ml
@@ -0,0 +1,64 @@
+(* The natural numbers, completed with [Infinity], and ordered towards
+   zero (i.e. [Infinity] is [bottom], [Finite 0] is [top]). *)
+
+type t =
+| Finite of int
+| Infinity
+
+type property =
+  t
+
+let equal p1 p2 =
+  match p1, p2 with
+  | Finite i1, Finite i2 ->
+      i1 = i2
+  | Infinity, Infinity ->
+      true
+  | _, _ ->
+      false
+
+let bottom =
+  Infinity
+
+let is_maximal p =
+  match p with
+  | Finite 0 ->
+      true
+  | _ ->
+      false
+
+let min p1 p2 =
+  match p1, p2 with
+  | Finite i1, Finite i2 ->
+      Finite (min i1 i2)
+  | p, Infinity
+  | Infinity, p ->
+      p
+
+let min_lazy p1 p2 =
+  match p1 with
+  | Finite 0 ->
+      p1
+  | _ ->
+      min p1 (Lazy.force p2)
+
+let add p1 p2 =
+  match p1, p2 with
+  | Finite i1, Finite i2 ->
+      Finite (i1 + i2)
+  | _, _ ->
+      Infinity
+
+let add_lazy p1 p2 =
+  match p1 with
+  | Infinity ->
+      Infinity
+  | _ ->
+      add p1 (Lazy.force p2)
+
+let print p =
+  match p with
+  | Finite i ->
+      string_of_int i
+  | Infinity ->
+      "infinity"
