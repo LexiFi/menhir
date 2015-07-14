a4c58c91333f4d8fd03824498cd9cf2d91c7f720
François Pottier <francois.pottier@inria.fr>
2015-07-14 12:49:48 +0200

    Removed [number2node].

--- src/LRijkstra.ml
+++ src/LRijkstra.ml
@@ -87,18 +87,6 @@ let foreach_terminal_not_causing_an_error s f =
 
 (* ------------------------------------------------------------------------ *)
 
-(* TEMPORARY
-let number2node : int -> Lr1.node =
-  let table = Array.make Lr1.n None in
-  Lr1.iter (fun node ->
-    table.(Lr1.number node) <- Some node
-  );
-  let table =
-    Array.map (function Some x -> x | None -> assert false) table
-  in
-  Array.get table
-*)
-
 let id x = x
 let some x = Some x
 
