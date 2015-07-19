2941109f946252689e5f7d1a8a39a25277699f8c
François Pottier <francois.pottier@inria.fr>
2015-07-19 14:28:57 +0200

    Improvement in coverage.sh.

--- src/coverage.sh
+++ src/coverage.sh
@@ -3,11 +3,15 @@
 # This script runs menhir on every grammar in ../bench/good,
 # aborting if it takes too long.
 
+TIMEOUT=30
+
 export OCAMLRUNPARAM=b
 make clean || exit 1
 make bootstrap || exit 1
 rm log
 for f in ../bench/good/*.mly ; do
   echo $f | tee -a log
-  (timeout 30 time _stage2/menhir.native -v $f) 2>> log
+  (timeout $TIMEOUT time _stage2/menhir.native -v -lg 1 -la 1 $f) 2>> log
 done
+echo "Number of grammars that could not be handled in $TIMEOUT seconds:"
+grep aborting log | wc -l
