981e2823037e28d6a1a08b3cf868165d6ac7c623
François Pottier <francois.pottier@inria.fr>
2015-08-29 15:08:18 +0200

    Cleanup.

--- demos/calc-inspection/calc.ml
+++ demos/calc-inspection/calc.ml
@@ -17,12 +17,23 @@ module E =
     (Parser.MenhirInterpreter)
     (CalcErrorReporting)
 
+(* Define a printer for explanations. We treat an explanation as if it
+   were just an item: that is, we ignore the position information that
+   is provided in the explanation. Indeed, this information is hard to
+   show in text mode. *)
+
 let print_explanation explanation =
-  (* not printing positions in the past, as they are hard to show in a
-     meaningful way in text mode *)
   P.print_item (E.item explanation)
 
-(* Initialize the lexer, and catch any exception raised by the lexer. *)
+let print_explanations startp explanations =
+  Printf.fprintf stderr
+    "At line %d, column %d: syntax error.\n"
+    startp.Lexing.pos_lnum
+    startp.Lexing.pos_cnum;
+  List.iter print_explanation explanations;
+  flush stderr
+
+(* The rest of the code is as in the [calc] demo. *)
 
 let process (line : string) =
   let lexbuf = Lexing.from_string line in
@@ -33,14 +44,7 @@ let process (line : string) =
   | Lexer.Error msg ->
       Printf.fprintf stderr "%s%!" msg
   | E.Error ((startp, _), explanations) ->
-      Printf.fprintf stderr
-        "At line %d, column %d: syntax error.\n"
-        startp.Lexing.pos_lnum
-        startp.Lexing.pos_cnum;
-      List.iter print_explanation explanations;
-      flush stderr
-
-(* The rest of the code is as in the [calc] demo. *)
+     print_explanations startp explanations
 
 let process (optional_line : string option) =
   match optional_line with
