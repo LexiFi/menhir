ed23b08c801d138ad4fa5bf1433d20e81f29df93
François Pottier <francois.pottier@inria.fr>
2015-09-21 17:11:28 +0200

    Fixes in coverage.sh.

--- src/coverage.sh
+++ src/coverage.sh
@@ -8,10 +8,10 @@ TIMEOUT=30
 export OCAMLRUNPARAM=b
 make clean || exit 1
 make bootstrap || exit 1
-rm log
+rm -f log
 for f in ../bench/good/*.mly ; do
   echo $f | tee -a log
-  (timeout $TIMEOUT time _stage2/menhir.native -v -lg 1 -la 1 $f) 2>> log
+  (timeout $TIMEOUT time _stage2/menhir.native -v -lg 1 -la 1 --list-errors $f) 2>> log
 done
 echo "Number of grammars that could not be handled in $TIMEOUT seconds:"
 grep aborting log | wc -l
