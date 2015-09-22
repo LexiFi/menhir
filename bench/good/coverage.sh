a1674b709d62fca9807ce1b2ba912b6d90ed2a31
François Pottier <francois.pottier@inria.fr>
2015-09-22 17:56:21 +0200

    Fixes in coverage.sh.

--- bench/good/coverage.sh
+++ bench/good/coverage.sh
@@ -3,22 +3,24 @@
 # This script runs menhir --list-errors on every grammar in bench/good,
 # aborting if it takes too long.
 
-TIMEOUT=2
+TIMEOUT=60
 
 export OCAMLRUNPARAM=b
 SRC=../../src
-MENHIR=../../src/_stage1/menhir.native
+MENHIR=../../src/_stage2/menhir.native
 
 # Compile Menhir.
-make -C $SRC || exit 1
+make -C $SRC clean || exit 1
+make -C $SRC bootstrap || exit 1
 # Remove any leftover output files.
 rm -f log lr.csv
 # Print the header of lr.csv. (This should be kept in sync with LRijkstra.ml.)
 echo "grammar,terminals,nonterminals,size,states,trie,facts,edges,time" > lr.csv
-# Try every grammar.
-for f in *.mly ; do
-  echo $f | tee -a log
-  (timeout $TIMEOUT $MENHIR -lg 1 -la 2 --list-errors $f) &>> log
+# Try every grammar. (Only the single-file grammars, that is.)
+for f in `ls *.mly | egrep -v '.*-([1-9]).mly'` ; do
+  echo "Now dealing with: $f" | tee -a log
+  (timeout $TIMEOUT $MENHIR -lg 1 -la 2 --list-errors --lalr $f) &>> log
+  echo "Done dealing with: $f" | tee -a log
 done
 echo "Number of grammars that could not be handled in $TIMEOUT seconds:"
 grep aborting log | wc -l
