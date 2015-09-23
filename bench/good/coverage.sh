e6b6bd4e8e49c6e46c03d1175bbf91fb65f5d05b
François Pottier <francois.pottier@inria.fr>
2015-09-23 07:45:46 +0200

    Fixes in coverage.sh.

--- bench/good/coverage.sh
+++ bench/good/coverage.sh
@@ -3,7 +3,7 @@
 # This script runs menhir --list-errors on every grammar in bench/good,
 # aborting if it takes too long.
 
-TIMEOUT=60
+TIMEOUT=120
 
 export OCAMLRUNPARAM=b
 SRC=../../src
@@ -19,8 +19,8 @@ echo "grammar,terminals,nonterminals,size,states,trie,facts,edges,time" > lr.csv
 # Try every grammar. (Only the single-file grammars, that is.)
 for f in `ls *.mly | egrep -v '.*-([1-9]).mly'` ; do
   echo "Now dealing with: $f" | tee -a log
-  (timeout $TIMEOUT $MENHIR -lg 1 -la 2 --list-errors --lalr $f) &>> log
-  echo "Done dealing with: $f" | tee -a log
+  (timeout $TIMEOUT $MENHIR -lg 1 -la 2 --list-errors --lalr $f) >>log 2>&1
+  echo "Done dealing with: $f" >> log
 done
 echo "Number of grammars that could not be handled in $TIMEOUT seconds:"
 grep aborting log | wc -l
