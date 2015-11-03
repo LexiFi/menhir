643b92ab156045426b75dd423d5ca68851f1fe28
François Pottier <francois.pottier@inria.fr>
2015-11-03 18:54:48 +0100

    Improve config.sh so that it works even if the Menhir executable has not been built yet.

--- quicktest/config.sh
+++ quicktest/config.sh
@@ -1,15 +1,16 @@
 SRC=../src
+# Make SRC an absolute path.
+# (We need at least MENHIR to be an absolute path.)
+if command -v greadlink >/dev/null ; then
+  SRC=`greadlink -f $SRC`
+else
+  SRC=`readlink -f $SRC`
+fi
+
 BUILD=$SRC/_stage1
 
-# An absolute path for Menhir.
 MENHIR=$BUILD/menhir.native
-if which greadlink >/dev/null ; then
-  MENHIR=`greadlink -f $MENHIR`
-else
-  MENHIR=`readlink -f $MENHIR`
-fi
 
 CALC=calc
 DATA=calc-data
-
 GENE=gene
