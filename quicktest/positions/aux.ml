46420dbcd99f4ade193819fab07198b5f4784aeb
François Pottier <francois.pottier@inria.fr>
2015-11-04 08:51:22 +0100

    Print startpos and endpos on distinct lines.

--- quicktest/positions/aux.ml
+++ quicktest/positions/aux.ml
@@ -1,10 +1,11 @@
 open Lexing
 
 let print nt startpos endpos =
-  Printf.printf "%s: startpos = %s/%03d, endpos = %s/%03d\n"
+  Printf.printf "%s: startpos = %s/%03d,\n%s:   endpos = %s/%03d\n"
     nt
     startpos.pos_fname
     startpos.pos_cnum
+    nt
     endpos.pos_fname
     endpos.pos_cnum
 
