05faa567d2a94ef45157c7642613caf2322515bb
François Pottier <francois.pottier@inria.fr>
2017-03-31 14:58:11 +0200

    New function [error_state].

--- src/Engine.ml
+++ src/Engine.ml
@@ -718,6 +718,13 @@ module Make (T : TABLE) = struct
     env1.stack == env2.stack &&
     number env1.current = number env2.current
 
+  let error_state checkpoint =
+    match checkpoint with
+    | HandlingError env ->
+        number env.current
+    | _ ->
+        invalid_arg "error_state expects HandlingError"
+
   (* ------------------------------------------------------------------------ *)
 
   (* Access to the position of the lookahead token. *)
