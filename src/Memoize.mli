448251f23e55d0d09478c7f0cc4b65997a5cae3d
François Pottier <francois.pottier@inria.fr>
2017-12-01 11:55:57 +0100

    New module [Memoize].

---
+++ src/Memoize.mli
@@ -0,0 +1,25 @@
+module type MEMOIZER = sig
+  (* A type of keys. *)
+  type key
+  (* A memoization combinator for this type. *)
+  val memoize: (key -> 'a) -> (key -> 'a)
+end
+
+module type IMPERATIVE_MAP = sig
+  (* A type of keys. *)
+  type key
+  (* A type of imperative maps. *)
+  type 'a t
+  (* Creation, insertion, lookup. *)
+  val create: int -> 'a t
+  val add: 'a t -> key -> 'a -> unit
+  val find: 'a t -> key -> 'a
+end
+
+module Make (M : IMPERATIVE_MAP) : MEMOIZER with type key = M.key
+
+module MakeViaMap (O : Map.OrderedType) : MEMOIZER with type key = O.t
+
+module MakeViaHashtbl (H : Hashtbl.HashedType) : MEMOIZER with type key = H.t
+
+module Int : MEMOIZER with type key = int
