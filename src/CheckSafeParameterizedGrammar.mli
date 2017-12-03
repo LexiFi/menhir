bcc68155b39b0e6bc26b0cc0f06810cf7e5f2ac7
François Pottier <francois.pottier@inria.fr>
2017-12-03 22:43:39 +0100

    Add Menhir headers to some recently added files.

--- src/CheckSafeParameterizedGrammar.mli
+++ src/CheckSafeParameterizedGrammar.mli
@@ -1,3 +1,16 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Menhir                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*              Yann Régis-Gianas, PPS, Université Paris Diderot              *)
+(*                                                                            *)
+(*  Copyright Inria. All rights reserved. This file is distributed under the  *)
+(*  terms of the GNU General Public License version 2, as described in the    *)
+(*  file LICENSE.                                                             *)
+(*                                                                            *)
+(******************************************************************************)
+
 (* [check] determines whether a parameterized grammar is safe for expansion,
    that is, whether the process of expanding away its parameterized symbols is
    guaranteed to terminate. *)
--- src/Unifier.mli
+++ src/Unifier.mli
@@ -1,3 +1,16 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Menhir                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*              Yann Régis-Gianas, PPS, Université Paris Diderot              *)
+(*                                                                            *)
+(*  Copyright Inria. All rights reserved. This file is distributed under the  *)
+(*  terms of the GNU General Public License version 2, as described in the    *)
+(*  file LICENSE.                                                             *)
+(*                                                                            *)
+(******************************************************************************)
+
 (* This module provides a simple-minded implementation of first-order
    unification over an arbitrary signature. *)
 
