c48822db630e5873dd951625f713747adccbedcf
François Pottier <francois.pottier@inria.fr>
2018-10-05 18:13:16 +0200

    Add a few missing headers.

--- src/Trie.mli
+++ src/Trie.mli
@@ -1,3 +1,16 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Menhir                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*              Yann Régis-Gianas, PPS, Université Paris Diderot              *)
+(*                                                                            *)
+(*  Copyright Inria. All rights reserved. This file is distributed under the  *)
+(*  terms of the GNU General Public License version 2, as described in the    *)
+(*  file LICENSE.                                                             *)
+(*                                                                            *)
+(******************************************************************************)
+
 open Grammar
 
 (* Suppose [s] is a state that carries an outgoing edge labeled with a
