6c2965886ad4a2f5eb33b9881652281df49ef3fb
François Pottier <francois.pottier@inria.fr>
2019-10-02 11:51:26 +0200

    Add missing headers.

--- src/Compatibility.mli
+++ src/Compatibility.mli
@@ -1,3 +1,16 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Menhir                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*              Yann Régis-Gianas, PPS, Université Paris Diderot              *)
+(*                                                                            *)
+(*  Copyright Inria. All rights reserved. This file is distributed under the  *)
+(*  terms of the GNU General Public License version 2, as described in the    *)
+(*  file LICENSE.                                                             *)
+(*                                                                            *)
+(******************************************************************************)
+
 (* The standard library function [String.escaped] in OCaml 4.02.3 depends
    on the operating system function [isprint] and therefore can have OS-
    dependent, locale-dependent behavior. This issue has been fixed in OCaml
