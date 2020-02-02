26b2a3dc62ba35f23a5eb47e6dd65d71ce299ce1
François Pottier <francois.pottier@inria.fr>
2020-02-02 10:37:06 +0100

    Reformulate LALR as an instance of Fix.DataFlow.

--- src/LALR.ml
+++ src/LALR.ml
@@ -23,151 +23,78 @@ type lr1state =
   Lr0.lr1state
 
 module Run () = struct
+let () = ()
 
 (* -------------------------------------------------------------------------- *)
 
-(* Since the LALR automaton has exactly the same states as the LR(0)
-   automaton, up to lookahead information, we can use the same state
-   numbers. *)
+(* The LALR automaton has exactly the same states as the LR(0) automaton, up
+   to lookahead information. Therefore, we can use the same state numbers.
+   Thus, the states and the transitions of the LALR automaton are the same as
+   those of the LR(0) automaton! *)
+
+(* This means that we have almost nothing to do: in fact, the only thing that
+   we have to do is compute a mapping of LR(0) nodes to LR(1) states. *)
+
+(* This computation can be viewed as a fixed point computation. In fact, it is
+   a special kind of fixed point computation: it can be viewed as a forward
+   data flow analysis where the graph is the LR(0) automaton and a property is
+   an LR(1) state. *)
 
 type node =
   int
 
-let n =
-  Lr0.n
+(* A property is an LR(1) state. The function [join] is used to merge the
+   contributions of multiple predecessor states. The function [leq] is used to
+   detect stabilization. *)
 
-(* Each node is associated with a state. This state can change during
-   construction as nodes are merged. *)
+module P = struct
+  type property = lr1state
+  let leq = Lr0.subsume
+  let join = Lr0.union
+end
 
-let states : lr1state option array =
-  Array.make n None
+(* The graph. *)
 
-(* -------------------------------------------------------------------------- *)
+module G = struct
 
-(* Output debugging information if [--follow-construction] is enabled. *)
+  type variable = node
+  type property = P.property
 
-let print_state (state : lr1state) =
-  Lr0.print_closure "" state
+  (* The root nodes are the entry nodes of the LR(0) automaton. The properties
+     associated with these nodes are given by the function [Lr0.start]. *)
 
-let print_ostate (ostate : lr1state option) =
-  match ostate with
-  | None ->
-      "<none>"
-  | Some state ->
-      print_state state
+  let foreach_root f =
+    ProductionMap.iter (fun _prod node ->
+      f node (Lr0.start node)
+    ) Lr0.entry
 
-let follow_transition
-  (source : node) (symbol : Symbol.t) (target : node) (state : lr1state)
-=
-  if Settings.follow then
-    Printf.fprintf stderr
-      "Examining transition out of state %d along symbol %s to state %d.\n\
-       Proposed target state:\n%s"
-      source
-      (Symbol.print symbol)
-      target
-      (print_state state)
+  (* The edges are the edges of the LR(0) automaton, and the manner in which
+     each edge contributes to the computation of a property is given by the
+     function [Lr0.transition]. *)
 
-let follow_state (msg : string) (node : node) (print : bool) =
-  if Settings.follow then
-    Printf.fprintf stderr
-      "%s: %d.\n%s\n"
-      msg
-      node
-      (if print then print_ostate states.(node) else "")
+  let foreach_successor node state f =
+    SymbolMap.iter (fun symbol (successor_node : node) ->
+      let successor_state : lr1state = Lr0.transition symbol state in
+      f successor_node successor_state
+    ) (Lr0.outgoing_edges node)
 
-(* -------------------------------------------------------------------------- *)
+end
 
-(* A stack of pending nodes, whose outgoing transitions must be reexamined. *)
+(* Run the data flow computation. *)
 
-(* Invariant: if a node is in the stack, then [states.(node)] is not [None]. *)
-
-let stack : node Stack.t =
-  Stack.create()
-
-(* The Boolean array [dirty] keeps track of which nodes are in the stack and
-   allows us to avoid pushing a node onto the stack when it is already in the
-   stack. *)
-
-let dirty : bool array =
-  Array.make n false
-
-let schedule node =
-  if not dirty.(node) then begin
-    dirty.(node) <- true;
-    Stack.push node stack
-  end
-
-(* -------------------------------------------------------------------------- *)
-
-(* [examine] examines a node that has just been taken out of the stack. Its
-   outgoing transitions are inspected. If a successor node is newly discovered
-   or updated, then it is scheduled or rescheduled for examination. *)
-
-let rec examine node =
-  (* Fetch the LR(1) state currently associated with this node. *)
-  let state : lr1state = Option.force states.(node) in
-  (* Inspect the node's outgoing transitions. *)
-  SymbolMap.iter (fun symbol (successor_node : node) ->
-    let successor_state : lr1state = Lr0.transition symbol state in
-    follow_transition node symbol successor_node successor_state;
-    inspect successor_node successor_state
-  ) (Lr0.outgoing_edges node)
-
-(* [inspect node state] ensures that the state currently associated with
-   [node] is at least [state]. If this requires an update of [states.(node)],
-   then [node] is (re)scheduled for examination. *)
-
-and inspect node state =
-  match states.(node) with
-  | None ->
-      (* [node] is newly discovered. *)
-      follow_state "Target state is newly discovered" node true;
-      states.(node) <- Some state;
-      schedule node
-  | Some current ->
-      (* [node] has been discovered earlier. *)
-      if Lr0.subsume state current then begin
-        (* It is unaffected. *)
-        follow_state "Target state is unaffected" node false
-      end
-      else begin
-        (* It is affected and must itself be scheduled. *)
-        states.(node) <- Some (Lr0.union state current);
-        follow_state "Growing existing state" node true;
-        schedule node
-      end
-
-(* -------------------------------------------------------------------------- *)
-
-(* The actual construction process. *)
-
-(* Populate the stack with the entry nodes. *)
-
-let () =
-  ProductionMap.iter (fun _prod node ->
-    states.(node) <- Some (Lr0.start node);
-    schedule node
-  ) Lr0.entry
-
-(* As long as the stack is nonempty, examine the nodes in it. *)
-
-let () =
-  try
-    while true do
-      let node = Stack.pop stack in
-      dirty.(node) <- false;
-      examine node
-    done
-  with Stack.Empty ->
-    ()
+module F = Fix.DataFlow.ForIntSegment(Lr0)(P)(G)
+  (* [solution : variable -> property option]. *)
+  (* Because every node is reachable, this function never returns [None]. *)
 
 (* -------------------------------------------------------------------------- *)
 
 (* Expose the mapping of nodes to LR(1) states. *)
 
+let n =
+  Lr0.n
+
 let states : lr1state array =
-  Array.map Option.force states
+  Array.init n (fun node -> Option.force (F.solution node))
 
 let state : node -> lr1state =
   Array.get states
