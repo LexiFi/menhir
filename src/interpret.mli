ed76ef329c6b67e75a86627c4ac3da3ef93ee181
François Pottier <francois.pottier@inria.fr>
2020-11-26 14:16:34 +0100

    Removed [default_message] from the API of the module Interpret.

--- src/interpret.mli
+++ src/interpret.mli
@@ -17,12 +17,6 @@
 
 val run: unit -> unit
 
-(* This default error message is produced by [--list-errors] when it creates a
-   [.messages] file, and is recognized by [--compare-errors] when it compares
-   two such files. *)
-
-val default_message: string
-
 (* [print_messages_item] displays one data item. The item is of the form [nt,
    sentence, target], which means that beginning at the start symbol [nt], the
    sentence [sentence] ends in an error in the target state given by [target].
