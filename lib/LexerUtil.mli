23875476de8aa9916d8c1cbc1893dcec0dae48bd
François Pottier <francois.pottier@inria.fr>
2020-12-01 16:14:45 +0100

    Headers.

--- lib/LexerUtil.mli
+++ lib/LexerUtil.mli
@@ -1,3 +1,16 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Menhir                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*              Yann Régis-Gianas, PPS, Université Paris Diderot              *)
+(*                                                                            *)
+(*  Copyright Inria. All rights reserved. This file is distributed under the  *)
+(*  terms of the GNU Library General Public License version 2, with a         *)
+(*  special exception on linking, as described in the file LICENSE.           *)
+(*                                                                            *)
+(******************************************************************************)
+
 open Lexing
 
 (* [init filename lexbuf] initializes the lexing buffer [lexbuf] so
