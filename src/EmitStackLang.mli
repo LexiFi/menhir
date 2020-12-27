0bc33a275ade9cb6d3887e6530679d72178ea197
François Pottier <francois.pottier@inria.fr>
2020-12-27 10:48:53 +0100

    Introduce EmitStackLang.

---
+++ src/EmitStackLang.mli
@@ -0,0 +1,27 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Menhir                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*              Yann Régis-Gianas, PPS, Université Paris Diderot              *)
+(*                                                                            *)
+(*  Copyright Inria. All rights reserved. This file is distributed under the  *)
+(*  terms of the GNU General Public License version 2, as described in the    *)
+(*  file LICENSE.                                                             *)
+(*                                                                            *)
+(******************************************************************************)
+
+(* This module creates a StackLang program, based on information about the
+   grammar and about the LR(1) automaton that is extracted from the modules
+   Grammar, Lr1, and Default. *)
+
+module Run () : sig
+  val program: StackLang.program
+  val entry: Lr1.node -> StackLang.label
+end
+
+(* These are registers that must be defined (with dummy values) in the initial
+   runtime environment in StackLangInterpreter. This is not very pretty, but
+   works. It is used as part of our testing infrastructure for StackLang. *)
+
+val required: StackLang.register list
