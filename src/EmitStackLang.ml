58d7d9a830c3df6808a11e0f6bc22530c2ae6318
François Pottier <francois.pottier@inria.fr>
2021-01-06 18:16:59 +0100

    EmitStackLang: push a new cell either in [goto] or in [run].

--- src/EmitStackLang.ml
+++ src/EmitStackLang.ml
@@ -158,6 +158,76 @@ let runpushes s =
 
 (* -------------------------------------------------------------------------- *)
 
+(* When a "goto" transition (that is, a transition labeled with a nonterminal
+   symbol) is taken, a new cell must be pushed onto the stack. Two possible
+   placements come to mind for the PUSH instruction:
+
+   - One convention, "goto pushes", is to place this instruction in the [goto]
+     subroutine, prior to the case analysis whose branches contain jumps
+     to [run] subroutines.
+
+   - Another convention, "run pushes", is to place this instruction at the
+     beginning of [run]. *)
+
+(* With "goto pushes", we emit one PUSH instruction per nonterminal symbol,
+   whereas with "run pushes", we emit one such instruction per state whose
+   incoming symbol is a nonterminal symbol. For each terminal symbol [nt],
+   there is usually more than one state whose incoming symbol is [nt]. Thus,
+   "goto pushes" is more economical in terms of code size.
+
+   "run pushes" could be viewed as the composition of "goto pushes" with an
+   optimization phase where PUSH instructions are moved into [case]
+   instructions and past [jump] instructions. However, this optimization could
+   be somewhat painful to implement and costly, as it requires every
+   predecessor of the jump target to exhibit a similar [push] instruction. So,
+   if "run pushes" is what is desired, then we prefer to produce the desired
+   code directly. *)
+
+(* Even if one wishes to optimize for code size, "run pushes" remains
+   interesting in the case where every [run] subroutine of interest contains a
+   POP instruction. Indeed, in that case, the PUSH and POP instructions cancel
+   out, so there is no cost in code size for choosing "run pushes", while there
+   is a gain in speed. *)
+
+(* [every_run_pops nt] tests whether every state whose incoming symbol is [nt]
+   performs a default reduction of a non-epsilon production. This condition
+   ensures that the [run] subroutine for every such state contains a POP
+   instruction. *)
+
+let every_run_pops nt =
+  Lr1.targets (fun accu _ target ->
+    accu &&
+    match Default.has_default_reduction target with
+    | Some (prod, _) ->
+        Production.length prod > 0
+    | None -> false
+  ) true (Symbol.N nt)
+
+(* As suggested above, if we are trying to optimize for code size, then we
+   choose "goto pushes", except in those places where "run pushes" entails
+   no penalty. Otherwise, we choose "run pushes" everywhere. *)
+
+let gotopushes : Nonterminal.t -> bool =
+  if Settings.optimize_for_code_size then
+    Nonterminal.tabulate (fun nt -> not (every_run_pops nt))
+  else
+    fun _nt -> false
+
+let runpushes s =
+  match Lr1.incoming_symbol s with
+  | Some (Symbol.T _) ->
+      true
+  | None ->
+      false
+  | Some (Symbol.N nt) ->
+      (* There is a jump from a [goto] subroutine to a [run] subroutine.
+         Exactly one of them must be in charge of pushing a new cell. Thus,
+         [runpushes s] must be [true] if and only if [gotopushes nt] is
+         [false]. *)
+      not (gotopushes nt)
+
+(* -------------------------------------------------------------------------- *)
+
 (* Conventional names. *)
 
 let lexer, lexbuf = (prefix "lexer", prefix "lexbuf")
