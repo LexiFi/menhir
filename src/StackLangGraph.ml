0c8ece52e22ec6de0e2ab6b3576192ac0fc61192
François Pottier <francois.pottier@inria.fr>
2021-01-08 16:37:55 +0100

    Introduce StackLangGraph.

---
+++ src/StackLangGraph.ml
@@ -0,0 +1,48 @@
+(******************************************************************************)
+(*                                                                            *)
+(*                                   Menhir                                   *)
+(*                                                                            *)
+(*                       François Pottier, Inria Paris                        *)
+(*              Yann Régis-Gianas, PPS, Université Paris Diderot              *)
+(*                                                                            *)
+(*  Copyright Inria. All rights reserved. This file is distributed under the  *)
+(*  terms of the GNU General Public License version 2, as described in the    *)
+(*  file LICENSE.                                                             *)
+(*                                                                            *)
+(******************************************************************************)
+
+open Dot
+open StackLang
+
+(* [uniq] transforms an arbitrary [iter] function into one that produces each
+   element at most once. *)
+
+let uniq iter =
+  let encountered = ref LabelSet.empty in
+  fun yield ->
+    iter begin fun label ->
+      if not (StringSet.mem label !encountered) then begin
+        encountered := StringSet.add label !encountered;
+        yield label
+      end
+    end
+
+let print program =
+  let module P = Dot.Print(struct
+
+    type vertex = label
+    let name label = label
+
+    let successors (f : ?style:style -> label:string -> vertex -> unit) label =
+      lookup label program.cfg
+      |> uniq StackLangTraverse.successors (fun target -> f ~label:"" target)
+
+    let iter (f : ?shape:shape -> ?style:style -> label:string -> vertex -> unit) =
+      program.cfg |> LabelMap.iter begin fun label _block ->
+        f ~shape:Box ~label label
+      end
+
+  end) in
+  let f = open_out (Settings.base ^ ".dot") in
+  P.print f;
+  close_out f
