4ae84d2bf2a575eaf3ad63b855b1b00bacf3aa64
François Pottier <francois.pottier@inria.fr>
2021-01-08 16:37:55 +0100

    Introduce StackLangGraph.

--- src/StackLangGraph.ml
+++ src/StackLangGraph.ml
@@ -34,7 +34,8 @@ let print program =
     let name label = label
 
     let successors (f : ?style:style -> label:string -> vertex -> unit) label =
-      uniq StackLangTraverse.successors (fun target -> f ~label:"" target) (lookup label program.cfg).block
+      uniq StackLangTraverse.successors (fun target -> f ~label:"" target)
+        (lookup label program.cfg).block
 
 
     let iter (f : ?shape:shape -> ?style:style -> label:string -> vertex -> unit) =
