832e984db6a9c1be11f001d634a2a280bdf76835
Emile Trotignon <emile.trotignon@gmail.com>
2021-04-02 10:42:17 +0200

    code duplication

--- src/StackLang/Program.ml
+++ src/StackLang/Program.ml
@@ -4,4 +4,8 @@ type t = program
 
 let map f program = {program with cfg= RegisterMap.map f program.cfg}
 
-let iter f program = RegisterMap.iter (f) program.cfg
+let mapi f program = {program with cfg= RegisterMap.mapi f program.cfg}
+
+let iter f program = RegisterMap.iter f program.cfg
+
+let filter pred program = {program with cfg=RegisterMap.filter pred program.cfg}
