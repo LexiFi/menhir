4354228c078e4ca101ead910f22b735eddaee24d
François Pottier <francois.pottier@inria.fr>
2021-05-03 08:03:41 +0200

    Doc comments.

--- src/stringMap.mli
+++ src/stringMap.mli
@@ -13,28 +13,23 @@
 
 include Map.S with type key = string
 
-(* [cardinal m] is the cardinal of the map [m]. *)
-
+(**[cardinal m] is the cardinal of the map [m]. *)
 val cardinal : 'a t -> int
 
-(* [restrict s m] restricts the domain of the map [m] to (its
+(**[restrict s m] restricts the domain of the map [m] to (its
    intersection with) the set [s]. *)
-
 val restrict: StringSet.t -> 'a t -> 'a t
 
-(* [filter pred m] restricts the domain of the map [m] to
+(**[filter pred m] restricts the domain of the map [m] to
    (key, value) couples that verify [pred]. *)
-
 val filter: (string -> 'a -> bool) -> 'a t -> 'a t
 
-(* [domain m] returns the domain of the map [m]. *)
-
+(**[domain m] returns the domain of the map [m]. *)
 val domain: 'a t -> StringSet.t
 
-(* [multiple_add k v m] adds the key-value pair [k, v] to the map [m],
+(**[multiple_add k v m] adds the key-value pair [k, v] to the map [m],
    which maps keys to *lists* of values. The list currently associated
    with [k] is extended with the value [v]. *)
-
 val multiple_add: key -> 'a -> 'a list t -> 'a list t
 
 (**[of_list] converts a list of key-value pairs to a map. *)
