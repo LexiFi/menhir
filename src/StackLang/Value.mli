6bd866f1379129979f3f69f38d5e64d69db2e7db
François Pottier <francois.pottier@inria.fr>
2021-05-22 17:57:25 +0200

    Tiny cleanup.

--- src/StackLang/Value.mli
+++ src/StackLang/Value.mli
@@ -3,13 +3,13 @@ open StackLangBasics
 val registers : value -> registers
 (**[registers v] is the set of the registers that appear in the value [v]. *)
 
-val registers_of_list : value list -> RegisterSet.t
+val registers_of_list : value list -> registers
 (**[registers_of_list vs] is the set of the registers that appear in
    the values [vs]. *)
 
 module Accu : sig
 
   (**The union of [accu] with [registers v]. *)
-  val registers : RegisterSet.t -> value -> RegisterSet.t
+  val registers : registers -> value -> registers
 
 end
