6970f07d60ac153dbda121d2f481d41797e35f65
François Pottier <francois.pottier@inria.fr>
2021-05-22 15:36:37 +0200

    Remove [Bindings.singleton].

--- src/StackLang/Bindings.mli
+++ src/StackLang/Bindings.mli
@@ -9,10 +9,7 @@ val empty : t
 (** An empty set of bindings. *)
 
 val is_empty : t -> bool
-(** [is_empty b] is [true] if [b] is empty. *)
-
-val singleton : register -> value -> t
-(** [singleton r v] Is the singleton binding [r <- v]. *)
+(** [is_empty bs] is [true] if [bs] is empty. *)
 
 val singleton_pattern : pattern -> value -> t
 (** [singleton_pat p v] Is the binding of a value to a pattern. *)
@@ -39,31 +36,26 @@ val remove_value : t -> value -> t
     shape [r := _] from s. *)
 
 val apply : t -> value -> value
-(** [apply s value] apply to rules of the substitution [s] to [value]
-    recursively.
-    [IReturn (apply s value)] is equivalent to [restore_defs s (IReturn value)]. *)
-
-(* val apply_registers : t -> registers -> registers *)
-(** [apply_registers s regs] returns the set of register [reg'] such that
-    there exists [reg] in [regs] such that [reg'] is referred to in
-    [apply s reg]. *)
+(** [apply bs v] applies the bindings [bs], viewed as a substitution, to the
+    value [v]. Thus, the instruction [IReturn (apply bs v)] is equivalent to
+    the instruction [defs bs (IReturn v)]. *)
 
 val compose : t -> t -> t
-(** [compose b1 b2] returns a bindings [b] such that :
-      [def b1 (def b2 block)] is equivalent to
-      [def b block] *)
+(** [compose bs1 bs2] returns a set of bindings [bs] such that
+      [def bs1 (def bs2 block)] is equivalent to
+      [def bs block] *)
 
 val domain : t -> registers
-(**[domain b] is the domain of [b], that is, the set of registers assigned by
-   [b]. *)
+(**[domain bs] is the domain of [bs], that is, the set of registers assigned
+   by [bs]. *)
 
 val codomain : t -> registers
 
 val restrict : t -> registers -> t
 
 val to_list : t -> (register * value) list
-(**[to_list b] is the set of bindings [b], viewed as a list of register/value
-   pairs, in an unspecified order. *)
+(**[to_list bs] is the set of bindings [bs], viewed as a list of
+   register/value pairs, in an unspecified order. *)
 
 val fold : (register -> value -> 'a -> 'a) -> t -> 'a -> 'a
 (** [fold] iterates over a set of bindings, in an unspecified order. *)
