f1e6d4d84057fce6ae5c63e0ba31ef2b5936126f
François Pottier <francois.pottier@inria.fr>
2021-05-22 17:07:06 +0200

    Keep just one function [Bindings.remove].

--- src/StackLang/Bindings.ml
+++ src/StackLang/Bindings.ml
@@ -18,60 +18,48 @@ let rec apply bs v =
   | VUnit
     -> v
 
-(* [add] is an internal function. *)
+(* [add] is an internal function; it is used in the definition of [assign]
+   and [compose]. *)
+
 let add r v bs =
   match v with
-  (* We avoid adding identity bindings. This is not essential, but may help
+  (* We drop identity bindings. This is not essential, but may help
      avoid a certain amount of noise. *)
   | VReg r' when r = r' ->
       bs
   | _ ->
       RegisterMap.add r v bs
 
-let extend bs r v =
-  add r (apply bs v) bs
+(* [assign] with an accumulator. *)
 
-let rec extend_pattern bs pattern value =
-  match (pattern, value) with
+let rec assign bs p v =
+  match p, v with
   | PWildcard, _ ->
       bs
-  | PReg reg, value ->
-      extend bs reg value
+  | PReg r, v ->
+      add r v bs
   | PTuple ps, VTuple vs ->
-      List.fold_left2 extend_pattern bs ps vs
+      assert (List.length ps = List.length vs);
+      List.fold_left2 assign bs ps vs
   | _ ->
       assert false
 
+(* [assign] without an accumulator. We assume that no register is assigned
+   twice by the pattern [p]. *)
+
+let assign p v =
+  assign empty p v
+
+(* To compute the sequential composition of [bs1] and [bs2], we must apply
+   [bs1] to each of the values in the codomain of [bs2]. We can then add
+   each binding in [bs2] to [bs1], possibly overriding previous bindings. *)
 
 let compose bs1 bs2 =
-  (* We fold on bs2 with an accumulator starting at bs1 *)
-  fold (fun reg value bs -> add reg (apply bs1 value) bs) bs2 bs1
+  (* We fold on [bs2] with an accumulator whose initial value is [bs1]. *)
+  fold (fun r v bs -> add r (apply bs1 v) bs) bs2 bs1
 
-
-let assign = extend_pattern empty
-
-let rec remove bs pattern =
-  match pattern with
-  | PReg reg ->
-      RegisterMap.remove reg bs
-  | PWildcard ->
-      bs
-  | PTuple ps ->
-      List.fold_left remove bs ps
-
-
-let remove_registers bs registers =
-  RegisterSet.fold RegisterMap.remove registers bs
-
-
-let rec remove_value bs = function
-  | VReg reg ->
-      remove bs (PReg reg)
-  | VTuple vs ->
-      List.fold_left remove_value bs vs
-  | _ ->
-      bs
-
+let remove bs rs =
+  RegisterSet.fold RegisterMap.remove rs bs
 
 let codomain bs =
   fold
