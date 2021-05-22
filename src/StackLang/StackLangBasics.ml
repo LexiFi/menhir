6c0bac9799552276c33549370ee19febd4c61dd4
François Pottier <francois.pottier@inria.fr>
2021-05-22 17:50:43 +0200

    Cleanup of StackLangBasics.

--- src/StackLang/StackLangBasics.ml
+++ src/StackLang/StackLangBasics.ml
@@ -1,17 +1,19 @@
-type register = string
+(* Registers. *)
+
+type register =
+  string
 
 module RegisterSet = StringSet
 module RegisterMap = StringMap
-module TagMap = MMap.Make (Int)
-
-module TagSet = TagMap.Domain
-
-
 type registers = RegisterSet.t
 
-(** A tag is an integer value. A tag can be used to encode a state of an LR
-   automaton. *)
-type tag = int
+(* Tags. *)
+
+type tag =
+  int
+
+module TagMap = MMap.Make (Int)
+module TagSet = TagMap.Domain
 
 let string_of_tag = string_of_int
 
@@ -19,26 +21,27 @@ let tag_of_node = Lr1.number
 
 let tag_of_int = Fun.id
 
-(** A code label is identified by its name. *)
-type label = string
+(* Labels. *)
 
-(** A terminal symbol. *)
-type terminal = Grammar.Terminal.t
+type label =
+  string
 
-(** A set of terminal symbols. *)
-type terminals = Grammar.TerminalSet.t
+(* Symbols. *)
 
-(** A value is a piece of data that can be pushed onto the stack. Values
-   include tags, data loaded from a register, and tuples of values. *)
+type terminal =
+  Grammar.Terminal.t
+
+type terminals =
+  Grammar.TerminalSet.t
+
+(* Values and patterns. *)
+
 type value =
+  | VUnit
   | VTag of tag
   | VReg of register
   | VTuple of value list
-  | VUnit
 
-(** A pattern describes how to decompose and store a piece of data that is
-   popped off the stack. Patterns include wildcards, registers, and tuples
-   of patterns. *)
 type pattern =
   | PWildcard
   | PReg of register
