1b8dae45cf97490f95ef29d20a9551bc8d9d4a36
François Pottier <francois.pottier@inria.fr>
2021-10-13 17:31:35 +0200

    Use [MList.ifnlazy].

--- src/tokenType.ml
+++ src/tokenType.ml
@@ -163,7 +163,7 @@ let produce_tokentypes grammar =
 
       let i =
         tokentypedef grammar @
-        ifnlazy Settings.inspection (fun () ->
+        MList.ifnlazy Settings.inspection (fun () ->
           tokengadtdef grammar
         )
       in
