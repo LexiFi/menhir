d740c62da84578476eff5f1d3e5ad5649eaf7489
François Pottier <francois.pottier@inria.fr>
2021-12-15 17:17:03 +0100

    Release 20211215.

--- coq-menhirlib/src/Version.v
+++ coq-menhirlib/src/Version.v
@@ -1,12 +1,1 @@
-(******************************************************************************)
-(*                                                                            *)
-(*                                   Menhir                                   *)
-(*                                                                            *)
-(*  Copyright Inria and CNRS. All rights reserved. This file is distributed   *)
-(*  under the terms of the GNU Lesser General Public License as published by  *)
-(*  the Free Software Foundation, either version 3 of the License, or (at     *)
-(*  your option) any later version, as described in the file LICENSE.         *)
-(*                                                                            *)
-(******************************************************************************)
-
-Definition require_unreleased := tt.
+Definition require_20211215 := tt.
