54d52309a33355d33f7bf4250404fa6e4f8440a8
François Pottier <francois.pottier@inria.fr>
2023-08-02 13:45:39 +0200

    Refactoring in promote.sh (which still does not work as desired).

--- promote.sh
+++ promote.sh
@@ -14,30 +14,23 @@ set -euo pipefail
 
 for name in "$@"
 do
+  # Construct the names of the expected-output files.
   if [[ $name =~ ^good/.* ]] ; then
     # A positive test.
-    echo "Promoting $name..."
-    # Create the expected-output files if they are missing.
-    touch test/static/$name.opp.exp
-    touch test/static/$name.exp
-    touch test/static/$name.automaton.exp
-    touch test/static/$name.automaton.resolved.exp
-    touch test/static/$name.conflicts.exp
-    base=${name#good/}
-    # Ask dune to update the expected-output files.
-    dune build @$base --auto-promote
+    expected="NAME.opp.exp NAME.exp NAME.automaton.exp NAME.automaton.resolved.exp NAME.conflicts.exp"
   elif [[ $name =~ ^bad/.* ]] ; then
     # A negative test.
-    echo "Promoting $name..."
-    # Create the expected-output file if it is missing.
-    touch test/static/$name.exp
-    base=${name#bad/}
-    # Ask dune to update the expected-output file.
-    dune build @$base --auto-promote
+    expected="NAME.exp"
   else
     # Unrecognized.
     echo "Don't know what to do with '$name'."
     echo "This script handles tests whose name begins with good/ or bad/."
     exit 1
   fi
+  expected="${expected//NAME/test/static/$name}"
+  # Ask dune to update the expected-output files.
+  echo "Promoting $name..."
+  dune build $expected --auto-promote
+    # TODO this command does not have the desired behavior
+    #      (to rebuild/promote just the files $expected)
 done
