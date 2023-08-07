0c8b3a590bf41b74daddbffaf05dcd525bd82a71
François Pottier <francois.pottier@inria.fr>
2023-08-07 22:01:20 +0200

    Typos in comments.

--- src/MList.mli
+++ src/MList.mli
@@ -57,8 +57,8 @@ val group_by :
   compare:('a -> 'a -> int) -> group:('a -> 'a list -> 'b) ->
   'a list -> 'b list
 
-(** [find_map f xs] applies [f] to elements of [xs] in order and the returns
-    the first result of the form [Some y], or [None]. *)
+(** [find_map f xs] applies [f] to the elements of [xs] in order and returns
+    the first result of the form [Some y], if there is one, or [None]. *)
 val find_map : ('a -> 'b option) -> 'a list -> 'b option
 
 (** [partition_map f xs] classifies elements of list [xs] in a left and a right
@@ -66,6 +66,6 @@ val find_map : ('a -> 'b option) -> 'a list -> 'b option
 val partition_map :
   ('a -> [< `L of 'l | `R of 'r ]) -> 'a list -> 'l list * 'r list
 
-(** [compare f l1 l2] compares two list according to the lexicographic
-    ordering. Elements are compared using the [f] argument. *)
+(** [compare f l1 l2] compares two lists according to the lexicographic
+    ordering. Elements are compared using the argument [f]. *)
 val compare : ('a -> 'b -> int) -> 'a list -> 'b list -> int
